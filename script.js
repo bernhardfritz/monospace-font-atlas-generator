c0bfbae24100887fdbf6e58b6ed292963e1753f5
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2026-04-02 23:11:25 +0200

    feat: reduce font atlas file size

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
-const width = 512;
-const height = 512;
+const rows = 6;
 const columns = 16;
 const gap = 1;
-let fontSize = 32;
-let fontFamily = 'monospace';
+const DEFAULT_FONT_SIZE = 32;
+const DEFAULT_FONT = `${DEFAULT_FONT_SIZE}px monospace`;
+let fontSize = DEFAULT_FONT_SIZE;
+let font = DEFAULT_FONT;
 const canvas = document.querySelector('#canvas');
-canvas.width = width;
-canvas.height = height;
 const ctx = canvas.getContext('2d');
-const fontFamilyInput = document.querySelector('#fontFamily');
-fontFamilyInput.value = fontFamily;
-fontFamilyInput.addEventListener('input', handleInput);
-downloadButton = document.querySelector('#download');
+const fontInput = document.querySelector('#fontInput');
+fontInput.value = font;
+fontInput.addEventListener('input', () => {
+  font = fontInput.value;
+  const regexMatch = font.match(/(\d+)px (.+)/);
+  if (regexMatch) {
+    fontSize = +regexMatch[1]; 
+  } else {
+    font = DEFAULT_FONT;
+    fontSize = DEFAULT_FONT_SIZE;
+  }
+  repaint();
+});
+downloadButton = document.querySelector('#downloadButton');
 downloadButton.addEventListener('click', () => {
   const dataUrl = canvas.toDataURL('image/png');
   const a = document.createElement('a');
   a.href = dataUrl;
-  a.download = `${fontSize}px ${fontFamily}.png`;
+  a.download = `${fontInput.value}.png`;
   a.click();
-  navigator.clipboard.writeText(`${ctx.measureText('a').width}`);
 });
-function handleInput() {
-  fontSize = 32;
-  fontFamily = fontFamilyInput.value || 'monospace';
+function repaint() {
+  ctx.font = font;
+  const monospaceWidth = ctx.measureText('a').width;
+  canvas.width = Math.ceil(gap + (monospaceWidth + gap) * columns);
+  canvas.height = Math.ceil(gap + (fontSize + gap) * rows);
   ctx.reset();
-  for (ctx.font = `${fontSize + 1}px ${fontFamily}`; gap + columns * (ctx.measureText('a').width + gap) < width; ctx.font = `${++fontSize + 1}px ${fontFamily}`) {}
-  ctx.font = `${fontSize}px ${fontFamily}`;
+  ctx.font = font;
   ctx.fillStyle = 'white';
-  for (let row = 0; row < 6; row++) {
+  for (let row = 0; row < rows; row++) {
     for (let col = 0; col < columns; col++) {
-      ctx.fillText(String.fromCharCode(32 + row * columns + col), gap + col * (ctx.measureText('a').width + gap), gap + fontSize + row * (gap + fontSize) - ctx.measureText(String.fromCharCode(...Array.from({ length: 95 }, (_, i) => 32 + i))).actualBoundingBoxDescent + 1) 
+      ctx.fillText(String.fromCharCode(32 + row * columns + col), gap + col * (ctx.measureText('a').width + gap), gap + fontSize + row * (gap + fontSize) - ctx.measureText(String.fromCharCode(...Array.from({ length: 95 }, (_, i) => 32 + i))).actualBoundingBoxDescent + 1);
     }
   }
-  // for (let row = 0; row < 6; row++) {
+  // for (let row = 0; row < rows; row++) {
   //   for (let col = 0; col < columns; col++) {
   //     ctx.strokeStyle = 'white';
-  //     ctx.strokeRect(1 + col * (ctx.measureText('a').width + 1), 1 + row * (fontSize + 1), ctx.measureText('a').width, fontSize);
+  //     ctx.strokeRect(gap + col * (ctx.measureText('a').width + gap), gap + row * (fontSize + gap), ctx.measureText('a').width, fontSize);
   //   }
   // }
 }
-handleInput();
+repaint();
